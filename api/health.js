af393f3395ab4654fed18bdd1e4c315a86597e98
strote34-arch <strote34@gmail.com>
2026-07-04 20:52:49 +0300

    Delete api/health.js

--- api/health.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({ ok: true, service: 'afishiru-api', ts: Date.now(), version: '1.0.0' });
-}
